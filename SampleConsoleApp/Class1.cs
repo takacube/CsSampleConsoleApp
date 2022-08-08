bae74e8ecfd354f1055db16514cb577d10c618a9
Takanao Shimakura <tashim@rinna.co.jp>
2022-08-09 07:13:58 +0900

    Add class

---
+++ SampleConsoleApp/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConsoleApp
+{
+    class Book
+    {
+        public string name;
+        public int id;
+
+        public void Buy()
+        {
+            Console.WriteLine("You sucessfully bought" + this.name);
+        }
+
+        public void Name()
+        {
+            Console.WriteLine("This books id is" + this.id);
+        }
+    }
+    internal class Class1
+    {
+    }
+}
--- SampleConsoleApp/Program.cs
+++ SampleConsoleApp/Program.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
-namespace User
-    {
+
+namespace SampleConsoleApp
+{
     class User
     {
         static void Main(string[] args)
@@ -20,6 +21,38 @@ namespace User
                 weights[i] = i;
                 Console.WriteLine(weights[i]);
             }
+            int[,] heights = { { 1, 2, 3 }, { 4, 5, 6 } };
+            CreateUser();
+            Shop("sampleItem");
+            int answer = Curicurate(8999);
+            Console.WriteLine(answer);
+
+            Book book1 = new Book();
+            book1.name = "C#  practice";
+            book1.id = 1;
+            Book book2 = new Book();
+            book2.name = "C# learning";
+            book2.id = 2;
+
+            book1.Buy();
+            book2.Name();
+        }
+
+        static void CreateUser()
+        {
+            Console.WriteLine("you have created user propery");
+        }
+
+        static void Shop(string itemName)
+        {
+            Console.WriteLine(itemName);            
+        }
+
+        static int Curicurate(int sample)
+        {
+            int answer;
+            answer = sample / 100;
+            return answer;
         }
     }
     }
